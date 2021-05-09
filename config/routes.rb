c7f59fc7d4936e64a8502221cbd19b7768211834
suguru <kashiwagura1051.1@gmail.com>
2021-05-09 19:07:24 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users,
+    controllers: {
+      registrations: 'registrations',
+      sessions: :sessions
+    }
+
+  root 'top#index'
+
+  resources :users, only: [:show, :index]
+  resources :reactions, only: [:create]
+  resources :matching, only: [:index]
+end
